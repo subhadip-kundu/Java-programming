8198f25ba7310acd8e7d526a77adab7abc2fd63a
Subhadip kundu <124190254+subhadip-kundu@users.noreply.github.com>
2024-03-29 15:31:14 +0530

    Add Author class along with one to one mapping

---
+++ book/src/main/java/com/bookapi/book/Entities/Author.java
@@ -0,0 +1,72 @@
+package com.bookapi.book.Entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "author_id")
+    private int authorId;
+
+    @Column(name = "First_name")
+    private String firstName;
+    private String lastName;
+    private String language;
+
+    public Author() {
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return "Author [authorId=" + authorId + ", firstName=" + firstName + ", lastName=" + lastName + ", language="
+                + language + "]";
+    }
+
+    public Author(int authorId, String firstName, String lastName, String language) {
+        this.authorId = authorId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.language = language;
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+}
